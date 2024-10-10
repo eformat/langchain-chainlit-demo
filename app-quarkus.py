eec37ca5c2096ab7988622a474284c63161006fb
Mike Hepburn <eformat@gmail.com>
2024-10-10 11:31:08 +1000

    🍅 support quarkus doc versions 🍅

--- app-quarkus.py
+++ app-quarkus.py
@@ -6,6 +6,7 @@ from chainlit.input_widget import Select, Switch, Slider
 from langchain.prompts import PromptTemplate
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain.chains import RetrievalQA
+from langchain.retrievers import MergerRetriever
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_openai import ChatOpenAI
 from langchain_community.vectorstores import PGVector
@@ -18,7 +19,6 @@ DB_CONNECTION_STRING = os.getenv(
     "DB_CONNECTION_STRING",
     "postgresql+psycopg://postgres:password@localhost:5432/vectordb",
 )
-DB_COLLECTION_NAME = os.getenv("DB_COLLECTION_NAME", "documents_test")
 
 template = "Q: {question} A:"
 
@@ -52,6 +52,12 @@ async def start_chat():
                 id="model_name",
                 label="OpenAI - Model",
                 values=["Meta-Llama-3.1-8B-Instruct-Q5_K_M.gguf"],
+                initial_index=0,
+            ),
+            Select(
+                id="collection_version",
+                label="Quarkus Document Version",
+                values=["3.15.1-Latest", "Main-SNAPSHOT", "3.8", "3.2", "2.16", "2.13", "2.7"],
                 initial_index=0,
             ),
             Slider(
@@ -94,14 +100,62 @@ async def start_chat():
 
     # Document store: pgvector vector store
     embeddings = HuggingFaceEmbeddings()
-    store = PGVector(
+    store_docs = PGVector(
         connection_string=DB_CONNECTION_STRING,
-        collection_name=DB_COLLECTION_NAME,
+        collection_name="docs",
         embedding_function=embeddings,
         use_jsonb=True,
     )
-    cl.user_session.set("store", store)
-
+    store_main = PGVector(
+        connection_string=DB_CONNECTION_STRING,
+        collection_name="main",
+        embedding_function=embeddings,
+        use_jsonb=True,
+    )
+    store_latest = PGVector(
+        connection_string=DB_CONNECTION_STRING,
+        collection_name="latest",
+        embedding_function=embeddings,
+        use_jsonb=True,
+    )
+    store_213 = PGVector(
+        connection_string=DB_CONNECTION_STRING,
+        collection_name="213",
+        embedding_function=embeddings,
+        use_jsonb=True,
+    )
+    store_216 = PGVector(
+        connection_string=DB_CONNECTION_STRING,
+        collection_name="216",
+        embedding_function=embeddings,
+        use_jsonb=True,
+    )
+    store_27 = PGVector(
+        connection_string=DB_CONNECTION_STRING,
+        collection_name="27",
+        embedding_function=embeddings,
+        use_jsonb=True,
+    )
+    store_38 = PGVector(
+        connection_string=DB_CONNECTION_STRING,
+        collection_name="38",
+        embedding_function=embeddings,
+        use_jsonb=True,
+    )
+    store_32 = PGVector(
+        connection_string=DB_CONNECTION_STRING,
+        collection_name="32",
+        embedding_function=embeddings,
+        use_jsonb=True,
+    )
+    cl.user_session.set("store_docs", store_docs)
+    cl.user_session.set("store_main", store_main)
+    cl.user_session.set("store_latest", store_latest)
+    cl.user_session.set("store_213", store_213)
+    cl.user_session.set("store_216", store_216)
+    cl.user_session.set("store_27", store_27)
+    cl.user_session.set("store_38", store_38)
+    cl.user_session.set("store_32", store_32)
 
 
 class StreamHandler(BaseCallbackHandler):
@@ -147,11 +201,59 @@ async def query_llm(message: cl.Message):
         http_client=httpx.Client(verify=False),
     )
 
-    store = cl.user_session.get("store")
+    store_docs = cl.user_session.get("store_docs")
+    store_main = cl.user_session.get("store_main")
+    store_latest = cl.user_session.get("store_latest")
+    store_213 = cl.user_session.get("store_213")
+    store_216 = cl.user_session.get("store_216")
+    store_27 = cl.user_session.get("store_27")
+    store_38 = cl.user_session.get("store_38")
+    store_32 = cl.user_session.get("store_32")
+
+    retriever_docs = store_docs.as_retriever(
+        search_type="similarity", search_kwargs={"k": 5}
+    )
+    retriever_main = store_main.as_retriever(
+        search_type="similarity", search_kwargs={"k": 5}
+    )
+    retriever_latest = store_latest.as_retriever(
+        search_type="similarity", search_kwargs={"k": 5}
+    )
+    retriever_213 = store_213.as_retriever(
+        search_type="similarity", search_kwargs={"k": 5}
+    )
+    retriever_216 = store_216.as_retriever(
+        search_type="similarity", search_kwargs={"k": 5}
+    )
+    retriever_27 = store_27.as_retriever(
+        search_type="similarity", search_kwargs={"k": 5}
+    )
+    retriever_38 = store_38.as_retriever(
+        search_type="similarity", search_kwargs={"k": 5}
+    )
+    retriever_32 = store_32.as_retriever(
+        search_type="similarity", search_kwargs={"k": 5}
+    )
+
+    lotr = None
+    if settings["collection_version"] == "3.15.1-Latest":
+        lotr = MergerRetriever(retrievers=[retriever_docs, retriever_latest])
+    elif settings["collection_version"] == "2.13":
+        lotr = MergerRetriever(retrievers=[retriever_docs, retriever_213])
+    elif settings["collection_version"] == "2.16":
+        lotr = MergerRetriever(retrievers=[retriever_docs, retriever_216])
+    elif settings["collection_version"] == "2.7":
+        lotr = MergerRetriever(retrievers=[retriever_docs, retriever_27])
+    elif settings["collection_version"] == "3.8":
+        lotr = MergerRetriever(retrievers=[retriever_docs, retriever_38])
+    elif settings["collection_version"] == "3.2":
+        lotr = MergerRetriever(retrievers=[retriever_docs, retriever_32])
+    else:
+        lotr = MergerRetriever(retrievers=[retriever_docs, retriever_main])
 
     llm_chain = RetrievalQA.from_chain_type(
         llm,
-        retriever=store.as_retriever(search_type="similarity", search_kwargs={"k": 5}),
+        retriever=lotr,
         chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
         return_source_documents=True,
         verbose=True,
